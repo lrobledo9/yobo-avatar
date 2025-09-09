849c68301cca4fe658a50981b0866b765a8f2f3e
lrobledo <lrobledo@onikom.com.mx>
2025-09-09 16:46:03 -0600

    n8n

---
+++ public/js/n8nService.js
@@ -0,0 +1,65 @@
+async function getQuestions(id) {
+
+    const data = await n8nService(id);
+
+    let questionTxt = '';
+    data.forEach(question => {
+        questionAux = JSON.parse(question.question)
+        questionTxt += `- ${questionAux.pregunta}\n`
+    });
+    console.log(questionTxt);
+
+    return { 'txt': questionTxt, 'length': data.length }
+}
+async function getVacant(id) {
+    const data = await n8nService(id);
+    console.log(data);
+    return {
+        vacant: data[0].title,
+        interviewid: data[0].interview_id,
+        application: data[0].application_id
+    };
+}
+
+async function sendDataInterview(id, url, chat) {
+    let body = {
+        "interviewId": id,
+        "videoLink": url,
+        "transcription": chat
+    }
+
+    const response = await fetch(`https://5pm84kkq-5678.usw3.devtunnels.ms/webhook/api/interview`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic WW9ib1NlcnZlcjphKDJGb3dYMDYtdzA="
+            },
+            body: JSON.stringify(body)
+
+        }
+    );
+
+    const data = await response;
+
+    if (data.status == 204) {
+        return 'OK';
+    }
+    return 'error';
+}
+
+
+async function n8nService(id) {
+
+    const response = await fetch(`https://5pm84kkq-5678.usw3.devtunnels.ms/webhook/api/interview?application_id=${id}`,
+        {
+            method: "GET",
+            headers: {
+                "Authorization": "Basic WW9ib1NlcnZlcjphKDJGb3dYMDYtdzA="
+            }
+        }
+    );
+
+    return await response.json();
+
+}
